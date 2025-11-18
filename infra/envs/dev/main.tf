ad32170abf38c9cf3b808e05d2599cabc2256784
Steve <10742399+sspier@users.noreply.github.com>
2025-11-18 13:56:56 -0500

    feat(infra): add VPC module with public/private subnets, IGW, NAT gateway, and routing (#10)
    Co-authored-by: Steven Spier <sspier@oswego.edu>

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -20,3 +20,20 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidrs = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_subnet_cidrs = [
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+  ]
+
+  tags = {
+    Project = "cloudpulse"
+    Env     = "dev"
+  }
+
+}
