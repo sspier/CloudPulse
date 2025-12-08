f62cd5f4e2b27d5d59ecee16a128a7126c75b057
Steven Spier <sspier@oswego.edu>
2025-12-08 18:13:27 -0500

    moved backend config from main.tf to backend.tf

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -10,15 +10,7 @@ terraform {
     }
   }
 
-  # store terraform state in s3 so multiple environments and users can share state safely
-  # locking prevents concurrent applies from corrupting state
-  backend "s3" {
-    bucket       = "cloudpulse-tf-state"
-    key          = "envs/dev/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true # encrypt state at rest
-    use_lockfile = true # enable state locking with s3 + dynamodb
-  }
+
 }
 
 # default aws provider for this environment
