0e66cc9bf6dbbf1ba5b4f62c134d7cd6c8acb42a
Steven Spier <sspier@oswego.edu>
2025-12-09 05:40:46 -0500

    Updated infra/envs/prod/main.tf to accept version 1.9.0

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -1,6 +1,6 @@
 terraform {
   # require a reasonably recent terraform version
-  required_version = ">= 1.11.0"
+  required_version = ">= 1.9.0"
 
   # aws provider pinned to a stable 5.x release
   required_providers {
@@ -8,16 +8,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
-
-  # store production state in s3 with locking enabled
-  # prod uses its own tfstate file to keep environments isolated
-  backend "s3" {
-    bucket       = "cloudpulse-tf-state"
-    key          = "envs/prod/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
   }
 }
 
